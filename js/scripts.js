9ac410c1e630d9ddedd81f0128ca4f38625fa3b4
Techmo27 <e.vins@yahoo.com>
2021-10-27 14:08:59 +0200

    Revert "Added br tag"
    This reverts commit d77b7cf04e8acc515de302374718930e42c5e519.

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ let pokemonList = [
 
 
 
-for (let i = 0 ; i < pokemonList[i].length; i++){
-  document.write(pokemonList[i].name + ' ' + 'height:' + pokemonList[i].height + "<br>");
+for (let i = 0 ;pokemonList[i]; i++){
+  document.write(pokemonList[i].name + ' ' + 'height:' + pokemonList[i].height + ' ');
 
 }
