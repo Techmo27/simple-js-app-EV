1db924ec13eaf1c6de2fad31513a59253dc0cdd4
Techmo27 <e.vins@yahoo.com>
2021-10-27 13:06:05 +0200

    Creation for loop for the pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,9 @@ let pokemonList = [
   {name: 'charmander', height: 5, type: 'fire'},
   {name: 'wartortle', height: 3, type: 'water'}
 ];
+
+
+
+for (let i = 0; i < pokemonList.length; i++){
+  document.write('<p>pokemonList[i].name + pokemonList[i].height + pokemonList[i].type</p>'')
+}
