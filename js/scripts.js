b7e98957c24e5ac049e987214b260a9c77f6170e
Techmo27 <e.vins@yahoo.com>
2021-11-06 13:10:49 +0100

    layout changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,29 @@
 //Array of Pokémons
 let pokemonList = [
-  {name: 'Bulbasur', height: 2.04, type: ['grass', 'poison']},
-  {name: 'Charmander', height: 2, type: 'fire'},
-  {name: 'Wartortle', height: 3.03, type: 'water'}
+  {
+    name: 'Bulbasur',
+    height: 2.04,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Charmander',
+    height: 2,
+    type: 'fire'
+  },
+  {
+    name: 'Wartortle',
+    height: 3.03,
+    type: 'water'
+  }
 ];
 
 
-/* // Loop through Pokémons
-for (let i = 0; i < pokemonList.length; i++){
-  // If-else statement
-  if (pokemonList[i].height > 3) {
-      document.write("<p>" + pokemonList[i].name + " " + "Height:" + " " + pokemonList[i].height + " " + "- Wow that's tall!<p>");
-  }else{
-      document.write("<p>" + pokemonList[i].name + " " + "Height:" + " " + pokemonList[i].height + "<p>");
-  }
-} */
-
 // looping through list of Pokémons
 pokemonList.forEach(function(pokemon) {
-  // If-else statement
+  // If-else statement to highlight Pokémons that are bigger than 3
   if (pokemon.height > 3) {
       document.write("<p>" + pokemon.name + " " + "Height:" + " " + pokemon.height + " " + "- Wow that's tall!<p>");
   }else{
       document.write("<p>" + pokemon.name + " " + "Height:" + " " + pokemon.height + "<p>");
   }
 });
-
-/*userList.forEach(function(user) {
-  console.log(user.name + ' is ' + user.age + ' years old.');
-});*/
