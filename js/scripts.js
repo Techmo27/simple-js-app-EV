5a932446594406a35393a54303dbba87b064c172
Techmo27 <e.vins@yahoo.com>
2021-11-06 13:31:55 +0100

    Additio IIFE and pokemon Repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,41 @@
-//Array of Pokémons
-let pokemonList = [
-  {
-    name: 'Bulbasur',
-    height: 2.04,
-    type: ['grass', 'poison']
-  },
-  {
-    name: 'Charmander',
-    height: 2,
-    type: 'fire'
-  },
-  {
-    name: 'Wartortle',
-    height: 3.03,
-    type: 'water'
+
+let pokemonRepository = (function () {
+
+  let pokemonList = [
+    {
+      name: 'Bulbasur',
+      height: 2.04,
+      type: ['grass', 'poison']
+    },
+    {
+      name: 'Charmander',
+      height: 2,
+      type: 'fire'
+    },
+    {
+      name: 'Wartortle',
+      height: 3.03,
+      type: 'water'
+    }
+  ];
+
+  function add(item)   {
+    pokemonList.push(item);
   }
-];
+
+  function getALL() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getALL: getALL
+  };
+
+})();
+
+pokemonRepository.getAll();
+pokemonRepository.add(item);
 
 
 // looping through list of Pokémons
