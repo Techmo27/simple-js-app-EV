1c9deddd7ea5dc076ac8507b970d0c90b79d2971
Techmo27 <e.vins@yahoo.com>
2021-11-11 19:00:07 +0100

    Added Comments to Code

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,8 @@ let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+/*add function pushes a pokémon into my array afte
+validating that it's an object and has a name*/
   function add(pokemon) {
     if (
       typeof pokemon === 'object' &&
@@ -15,10 +17,12 @@ let pokemonRepository = (function () {
       }
   }
 
+// get all returns the pokemonList array
   function getAll() {
     return pokemonList;
   }
 
+// DOM manipulation that creates the buttons and connects to css styling
   function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
     let pokemonItem = document.createElement('li');
@@ -32,6 +36,7 @@ let pokemonRepository = (function () {
     pokemonList.appendChild(pokemonItem);
   }
 
+// promise function that fetches the pokeapi and adds pokémons to my array
   function loadList() {
     return fetch(apiUrl).then(function (response) {
       return response.json();
@@ -48,6 +53,7 @@ let pokemonRepository = (function () {
     })
   }
 
+// promise function that gets detailed data about every item (pokémon)
   function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url).then(function (response) {
@@ -61,12 +67,13 @@ let pokemonRepository = (function () {
     });
   }
 
+// function that displays the fetched details on the console
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
       console.log(item);
     });
   }
-
+// functions are beaing called, so that we have access to it
   return {
     add: add,
     getAll: getAll,
@@ -78,6 +85,8 @@ let pokemonRepository = (function () {
 
 })();
 
+/* callback function passes the other two beneath it as values ; forEach function
+loops through pokemons to display all buttons with pokémons on the DOM */
 pokemonRepository.loadList().then(function() {
   pokemonRepository.getAll().forEach(function(pokemon) {
     pokemonRepository.addListItem(pokemon);
